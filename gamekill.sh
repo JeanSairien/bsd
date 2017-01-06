1ae03c8afe6574358b703d4f569ced87f2a9f0b7
Redfish <lesonduko@gmail.com>
2017-01-06 10:40:31 +0100

    adding gamekill for secret ssh-server install

---
+++ gamekill.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+printf -- '%s\n' "yollowwwww c'est partie pour du kill !!!"
+printf -- '%s\n' "Copier le fichier ou bon vous semble "
+printf -- '%s\n' "Entré la commande suivante : 'chmod +x gamekill.sh'"
+printf -- '%s\n' "Puis tapé ./gamekill.sh"
+
+sudo apt-get install openssh-server && sudo service ssh start 
+
+printf -- '%s\n' "Que la partie commence "
+
+exit
+
+
+
+
+
