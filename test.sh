b586cdbbf45737205d5b2538d900ecfe2a58f260
Redfish <lesonduko@gmail.com>
2017-01-06 09:38:31 +0100

    add bash lesson

--- test.sh
+++ test.sh
@@ -1,1 +1,44 @@
-/bin/sh
+#!/bin/sh
+
+
+
+#printf -- '%s\n' "bon alors ce printf "
+
+#printf -- "a-%s" "test "
+
+echo $?
+
+TOTO=yollow_les_totowww
+
+#sh -c
+
+
+	echo  "toto dit: "$TOTO"; return 1"
+
+		if test "$TOTO"
+                then 
+
+		sl
+			else  
+		echo "you lose"
+		sleep 3 
+		echo "noob"
+			#cat  test.sh | sed 's/e/a/g' 
+		exit 1
+	fi
+	shift
+done
+
+echo "c'est terminer !! "
+
+
+VALUE="1 2 3 4 5 6 7 8 9"
+
+try(){
+while ["$VALUE"]
+  do
+	i="${1}"
+	shift
+	echo ${1}
+  done 
+}
